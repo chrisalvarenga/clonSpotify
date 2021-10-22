e709d63406f790b43caf6517a52fab922d292656
chrisalvarenga <coalvarenga@unah.hn>
2021-10-22 06:31:46 +0200

    media-player

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CookieService } from 'ngx-cookie-service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InjectSessionInterceptor } from './core/interceptors/inject-session.interceptor';
 import { FavoritePageComponent } from './modules/favorites/pages/favorite-page/favorite-page.component';
 
 @NgModule({
@@ -15,7 +17,12 @@ import { FavoritePageComponent } from './modules/favorites/pages/favorite-page/f
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [CookieService,
+  {
+    provide: HTTP_INTERCEPTORS,
+    useClass: InjectSessionInterceptor,
+    multi:true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/modules/history/services/search.service.ts
+++ src/app/modules/history/services/search.service.ts
@@ -1,25 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
+export class SearchService {
   private readonly URL = environment.api
 
   constructor(private http: HttpClient) { }
 
-sendCredentials(email: string, password: string): Observable<any> {
-  const body = {
-    email,
-    password
+  searchTracks$(term: string): Observable<any> {
+    return this.http.get(`${this.URL}/tracks?src=${term}`)
+    .pipe(
+      map((dataRaw: any) => dataRaw.data)
+    )
   }
-  return this.http.post(`${this.URL}/auth/login`, body)
-}
 
-suma(a: number, b: number): number {
-  return a + b
-}
 }
